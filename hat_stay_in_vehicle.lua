031f20b865164cccd2cdd862f8070f6f92f6e58c
Le Moon Studio <97724060+LunaLeMoon@users.noreply.github.com>
2023-06-01 03:03:55 -0400

    Update hat_stay_in_vehicle.lua
    The first version of the script did not work. I realized that it may be because of two things:
    1. Hat prop index: In the line local currentProp = GetPedPropIndex(playerPed, 0), I'd then need to make sure that the hat prop index (0) matches the correct prop index for the hat used in-game. In other words, if the hat prop index is different, I would then need to adjust this value accordingly. And honestly, no. That would be too much work, and it is inefficient. 
    
    2. Hat prop preservation: The SetPedPropIndex function does not guarantee the hat prop when entering a vehicle. I realized that this is due to limitations of the game's mechanics, so I'd need an alternative.
    
    So, what's different? In this updated version, the AttachHatToPlayer function uses the GetPropModelName function to get the model name of the current hat prop. It then uses GetHashKey to get the hash value of the prop model dynamically. This way, the script can attach any hat prop that the player is wearing.
    
    But, that does mean that some hat props might not attach 'correctly' to the player's character model due to their size or position. This might need adjustments to the attachment position parameters (0.09, 0.06, 0.12) in the AttachEntityToEntity function to ensure the hat is properly aligned and positioned on the character's head bone.
    
    Hopefully this works better.

--- hat_stay_in_vehicle.lua
+++ hat_stay_in_vehicle.lua
@@ -1,5 +1,6 @@
 local wasInVehicle = false
-local hatProp = -1
+local hatProp = 0
+local hatObject = nil
 
 Citizen.CreateThread(function()
     while true do
@@ -15,14 +16,57 @@ Citizen.CreateThread(function()
 
             if currentProp ~= -1 then
                 hatProp = currentProp -- Store the hat prop
+                AttachHatToPlayer(playerPed, hatProp) -- Attach the hat prop to the player
             end
         elseif wasInVehicle and not inVehicle then
             wasInVehicle = false -- Left vehicle
 
-            -- Re-equip the hat prop if it was previously stored
-            if hatProp ~= -1 then
-                SetPedPropIndex(playerPed, 0, hatProp, 0, true)
-            end
+            -- Remove the attached hat prop
+            DetachHatFromPlayer()
         end
     end
 end)
+
+function AttachHatToPlayer(playerPed, hatProp)
+    if not hatObject then
+        local boneIndex = GetPedBoneIndex(playerPed, 0x39D7) -- Head bone index (SKEL_Head)
+
+        if boneIndex ~= -1 then
+            local propHash = GetHashKey(GetPropModelName(hatProp))
+
+            RequestModel(propHash)
+            while not HasModelLoaded(propHash) do
+                Citizen.Wait(100)
+            end
+
+            hatObject = CreateObject(propHash, 0.0, 0.0, 0.0, true, true, true)
+            AttachEntityToEntity(hatObject, playerPed, boneIndex, 0.09, 0.06, 0.12, 0.0, 0.0, 180.0, false, false, false, false, 2, true)
+            SetModelAsNoLongerNeeded(propHash)
+        end
+    end
+end
+
+function DetachHatFromPlayer()
+    if hatObject then
+        DeleteEntity(hatObject)
+        hatObject = nil
+    end
+
+    if hatProp ~= 0 then
+        SetPedPropIndex(PlayerPedId(), 0, hatProp, 0, true)
+        hatProp = 0
+    end
+end
+
+function GetPropModelName(prop)
+    local propHash = GetHashKey(GetPedPropTextureIndex(PlayerPedId(), 0))
+
+    if propHash ~= 0 then
+        local modelName = GetHashKey(GetEntityModel(propHash))
+        if modelName ~= 0 then
+            return GetModelNameFromHash(modelName)
+        end
+    end
+
+    return ""
+end
