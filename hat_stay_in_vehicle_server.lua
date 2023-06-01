704e5b8c926c569b9485ddd0c3d34ab3e8433e95
Le Moon Studio <97724060+LunaLeMoon@users.noreply.github.com>
2023-06-01 13:45:00 -0400

    Update hat_stay_in_vehicle_server.lua

--- hat_stay_in_vehicle_server.lua
+++ hat_stay_in_vehicle_server.lua
@@ -1,19 +1,9 @@
 RegisterServerEvent('attachHatToPlayer')
-AddEventHandler('attachHatToPlayer', function(hatProp)
-    TriggerClientEvent('attachHatToPlayer', -1, source, hatProp) -- Broadcast hat prop to all clients
+AddEventHandler('attachHatToPlayer', function(prop)
+    TriggerClientEvent('attachHatToPlayer', -1, prop) -- Broadcast hat prop to all clients
 end)
 
 RegisterServerEvent('detachHatFromPlayer')
 AddEventHandler('detachHatFromPlayer', function()
-    TriggerClientEvent('detachHatFromPlayer', -1, source) -- Broadcast hat removal to all clients
-end)
-
-RegisterNetEvent('attachHatToPlayer')
-AddEventHandler('attachHatToPlayer', function(source, hatProp)
-    TriggerClientEvent('attachHatToPlayer', -1, source, hatProp) -- Broadcast hat prop to all clients except the sender
-end)
-
-RegisterNetEvent('detachHatFromPlayer')
-AddEventHandler('detachHatFromPlayer', function(source)
-    TriggerClientEvent('detachHatFromPlayer', -1, source) -- Broadcast hat removal to all clients except the sender
+    TriggerClientEvent('detachHatFromPlayer', -1) -- Broadcast hat removal to all clients
 end)
