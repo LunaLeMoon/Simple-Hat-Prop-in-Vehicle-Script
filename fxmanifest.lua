d1defc97b86c93c4427d53e0be6988725c160d42
Le Moon Studio <97724060+LunaLeMoon@users.noreply.github.com>
2023-06-02 02:40:47 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,9 +3,9 @@ games { 'rdr3', 'gta5' }
 
 author 'LeMoon Studio'
 description 'Simple script that allows the player to keep their hat/helmet prop equipped when in a vehicle.'
-version '1.0.2'
+version '1.0.4'
 
 client_scripts {
-    'hat_stay_in_vehicle.lua'
+    'hat_stay_in_vehicle_client.lua'
 }
 server_script 'hat_stay_in_vehicle_server.lua'
